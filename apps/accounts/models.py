673733931fee7dc74ec1de8a9b79d48f1cbe8cc4
abuwcheek <abdulloistamov1034@gmail.com>
2025-03-02 16:08:49 +0500

    project added, customuser added

---
+++ apps/accounts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from apps.base.models import BaseModel
+
+
+
+class CustomUser(AbstractUser):
+
+     GENDER_CHOICES = (
+          ('JINS', 'Jins'),
+          ('AYOL', 'Ayol'),
+          ('ERKAK', 'Erkak'),
+     )
+
+     birth_date = models.DateField(null=True, blank=True)
+     email = models.EmailField(unique=True, primary_key=True)
+     phono_numara = models.CharField(max_length=15, null=True, blank=True)
+     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='JINS')
+
+
+     def __str__(self):
+          return f'{self.first_name} {self.last_name} {self.email}'
